998624aa352019cc997959dd7dbf57150334c28e
Awais Hassan <55058852+AWAIS97@users.noreply.github.com>
2020-06-26 15:26:40 +0500

    Added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Title from "../Title"
+import CartColumns from "./CartColumns"
+import EmtyCart from './EmptyCart'
+import { ProductConsumer } from '../../Context'
+import CartList from './CartList'
+import CartTotal from './CartTotal'
+
+export default class Cart extends Component {
+    render() {
+        return (
+            <section>
+                <ProductConsumer>
+                    {value => {
+                        const { cart } = value;
+                        if (cart.length > 0) {
+                            return (
+                                <React.Fragment>
+                                    <Title name='Your' title='Cart' />
+                                    <CartColumns />
+                                    <CartList value={value} />
+                                    <CartTotal value={value} />
+
+                                </React.Fragment>
+                            )
+                        }
+                        else {
+                            return <EmtyCart />
+                        }
+                    }}
+                </ProductConsumer>
+
+
+
+            </section>
+        )
+    }
+}
